c49b181160143e7816bf444a430548c86e50b382
Felipe Christian C <felipechristiab@iMac-de-Felipe.local>
2016-12-15 14:45:56 +0100

    Pacman, colisión.

--- PACMAN/pacman.js
+++ PACMAN/pacman.js
@@ -1,11 +1,12 @@
-var game = new Phaser.Game(450,500, Phaser.AUTO, 'phaser-example', {init:init, preload: preload, create: create, update:update});
+var game = new Phaser.Game(450,500, Phaser.AUTO, 'phaser-example', {init:init, preload: preload, create: create, update:update, mov :mov});
 
 var map;
 var layer;
 
 var pacman = null;
-
+var safetile = 14;
 var current = Phaser.NONE;
+
 
 function init() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -23,7 +24,9 @@ function preload() {
 }
 
 function create() {
+
     game.stage.backgroundColor = '#000000';
+
     map = game.add.tilemap('Map');
     map.addTilesetImage('pacman-mapa1', 'tiles');
     layer = map.createLayer('mapa1');
@@ -31,6 +34,9 @@ function create() {
     // Posición del pacman, grid location 14x17
     pacman = game.add.sprite((14 * 16) + 8, (17 * 16) + 8, 'pacman', 0);
     pacman.anchor.set(0.5);
+
+    // Pacman should collide with everything except the safe tile
+    map.setCollisionByExclusion([safetile], true, layer);
 
     //Nombre animacion, Frames, Velocidad por frame y si es ciclico.
     pacman.animations.add('munch', [0, 1, 2, 1], 20, true);
@@ -46,15 +52,20 @@ function create() {
 
 
 function update() {
+    game.physics.arcade.collide(pacman, layer);
+    mov();
+}
 
-    if (cursors.left.isDown && current !== Phaser.LEFT) {
+function mov(){
+
+    if(cursors.left.isDown && current!== Phaser.LEFT){
 
         pacman.angle = 180;
         pacman.body.velocity.x = -150;
         pacman.animations.play('left');
 
     }
-    else if (cursors.right.isDown && current !== Phaser.RIGHT) {
+    else if(cursors.right.isDown && current!== Phaser.RIGHT){
 
         pacman.angle = 360;
         pacman.body.velocity.x = 150;
@@ -73,6 +84,7 @@ function update() {
         pacman.body.velocity.y = 150;
         pacman.animations.play('down');
     }
+
 }
 
 
