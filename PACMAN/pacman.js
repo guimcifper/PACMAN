4fb83705a3cb883b1bcee925cf9fcebb439c54af
Felipe Christian C <felipechristiab@iMac-de-Felipe.local>
2016-12-15 13:51:39 +0100

    Animación Pacman

--- PACMAN/pacman.js
+++ PACMAN/pacman.js
@@ -1,9 +1,9 @@
-
 var game = new Phaser.Game(450,500, Phaser.AUTO, 'phaser-example', {preload: preload, create: create});
 
 var map;
 var layer;
 var pacman = null;
+
 
 function preload() {
     game.load.tilemap('Map', 'Map.json', null, Phaser.Tilemap.TILED_JSON);
@@ -18,8 +18,18 @@ function create() {
     map.addTilesetImage('pacman-mapa1', 'tiles');
     layer = map.createLayer('mapa1');
 
+    // Posición del pacman, grid location 14x17
     pacman = game.add.sprite((14 * 16) + 8, (17 * 16) + 8, 'pacman', 0);
     pacman.anchor.set(0.5);
+
+    //Nombre animacion, Frames, Velocidad por frame y si es ciclico.
+    pacman.animations.add('munch', [0, 1, 2, 1], 20, true);
+
+    game.physics.arcade.enable(pacman);
+    pacman.body.setSize(16, 16, 0, 0);
+
+    //animación
+    pacman.play('munch');
 
 }
 
