03791f64df2fb4700cc9e5c0649a8ada85b00926
Felipe Christian C <felipechristiab@iMac-de-Felipe.local>
2016-12-15 13:35:14 +0100

    Mostrar pacman

--- PACMAN/pacman.js
+++ PACMAN/pacman.js
@@ -3,7 +3,7 @@ var game = new Phaser.Game(450,500, Phaser.AUTO, 'phaser-example', {preload: pre
 
 var map;
 var layer;
-
+var pacman = null;
 
 function preload() {
     game.load.tilemap('Map', 'Map.json', null, Phaser.Tilemap.TILED_JSON);
@@ -17,6 +17,9 @@ function create() {
     map = game.add.tilemap('Map');
     map.addTilesetImage('pacman-mapa1', 'tiles');
     layer = map.createLayer('mapa1');
+
+    pacman = game.add.sprite((14 * 16) + 8, (17 * 16) + 8, 'pacman', 0);
+    pacman.anchor.set(0.5);
 
 }
 
